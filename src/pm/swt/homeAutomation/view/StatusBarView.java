8770ab91866642659790da486c78fe19ecef59fa
Plamen Marinov <44176168+PMarinov1994@users.noreply.github.com>
2021-05-07 20:37:14 +0300

    Update StatusBarView.java

--- src/pm/swt/homeAutomation/view/StatusBarView.java
+++ src/pm/swt/homeAutomation/view/StatusBarView.java
@@ -10,18 +10,13 @@ import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowData;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 
-import javafx.animation.Timeline;
 import pm.swt.homeAutomation.viewModel.StatusBarViewModel;
 
 
